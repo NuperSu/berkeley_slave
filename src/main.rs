bcbe8709fd410dea6e348fed66b56414d3097200
NuperSu <antonkosovsk@gmail.com>
2024-03-20 17:47:26 +0200

    Added more output

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
 
     let slave_address = args.nth(1).unwrap();
+    println!("Slave node starting. Binding to address: {}", slave_address);
     let socket = UdpSocket::bind(&slave_address).await?;
     println!("Slave node running on {}", slave_address);
 
@@ -25,6 +26,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).await?;
         let received_message = String::from_utf8_lossy(&buf[..number_of_bytes]).into_owned();
 
+        println!("Received message from {}: {}", src_addr, received_message);
         handle_message(received_message, &src_addr, &socket, &time_keeper).await?;
         sleep(Duration::from_secs(5)).await; // Customize based on how frequently you want to update or respond
     }
